9585c1c6e1614a2acaaa5d5976d574085fca05f6
Mario Monaco <monaco@MacBook-Pro-de-Mario.local>
2017-07-29 19:40:15 -0300

    Subida

---
+++ mean/modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -0,0 +1,39 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('users')
+    .controller('EditProfileController', EditProfileController);
+
+  EditProfileController.$inject = ['$scope', '$http', '$location', 'UsersService', 'Authentication', 'Notification', '$state'];
+
+  function EditProfileController($scope, $http, $location, UsersService, Authentication, Notification, $state) {
+    var vm = this;
+
+    vm.user = Authentication.user;
+    vm.updateUserProfile = updateUserProfile;
+
+    // Update a user profile
+    function updateUserProfile(isValid) {
+
+      if (!isValid) {
+        $scope.$broadcast('show-errors-check-validity', 'vm.userForm');
+
+        return false;
+      }
+
+      var user = new UsersService(vm.user);
+
+      user.$update(function (response) {
+        $scope.$broadcast('show-errors-reset', 'vm.userForm');
+
+        Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Edit profile successful!' });
+        Authentication.user = response;
+        // And redirect to the previous or home page
+        $state.go($state.previous.state.name || 'home', $state.previous.params);
+      }, function (response) {
+        Notification.error({ message: response.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Edit profile failed!' });
+      });
+    }
+  }
+}());
